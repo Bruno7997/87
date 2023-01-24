b11c325216af1f400d828b0351ca629b0fd74a30
Bruno7997 <100882698+Bruno7997@users.noreply.github.com>
2023-01-24 17:01:32 -0300

    Add files via upload

---
+++ screens/DailyPic.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react"
+import {Text, View} from "react-native"
+
+export default class DailyPic extends Component{
+    render(){
+        return(
+            <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+<Text>Tela da nave espacial</Text>
+            </View>
+        )
+    }
+}
